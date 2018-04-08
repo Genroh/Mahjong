e2214243ab4b7f4778c249e942ac287102915393
Genroh <lightningrod.marowak.105@gmail.com>
2018-04-08 14:14:55 +0900

    配牌しました

--- gametest.py
+++ gametest.py
@@ -25,11 +25,29 @@ def scene_game(players):
 
 
 if __name__ == '__main__':
+    lst = hantei.lst * 4
+
     print("Input your name.")
     players = []
     players.append(Player(input()))
     players.append(Player("CPU1"))
     players.append(Player("CPU2"))
     players.append(Player("CPU3"))
+
+    if sys.platform == 'win32':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+    for i in range(14):
+        print(f"{i:02}", end=" ")
+    print('\n')
+    yama = hantei.rndlst(lst)
+    for i in range(4):
+        players[i].tehai.extend(sorted(yama[0:13]))
+        del yama[0:13]
+        print(*[hantei.dic[x] for x in players[i].tehai])
+        print()
+    print()
 
     scene_game(players)
