2b2eae968c98f87e16a37c1bc8ed679b81b4ff93
Genroh <lightningrod.marowak.105@gmail.com>
2018-04-15 02:52:25 +0900

    ひどかったゲームループを書き直して一人ずつ動くように

--- gametest.py
+++ gametest.py
@@ -16,7 +16,17 @@ class Game:
         self.oya = -1
         self.ba = -1
         self.kyoku = 0
+        self.honba = -1
+
+    def init(self):
+        self.turn = random.randint(0, 3)
+        self.oya = self.turn
+        self.ba = 0
+        self.kyoku = 1
         self.honba = 0
+
+    def next(self):
+        self.turn = (self.turn + 1) % 4
 
 
 class Player:
@@ -37,6 +47,21 @@ def used_hai(players):
 
 
 def scene_game(players):
+    if sys.platform == 'win32':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+    print(f"{hantei.dic[game.ba+41]}{game.kyoku}局 {game.honba}本場")
+    print(f"山残り : {len(yama)-14}")
+    for i in range(14):
+        print(f"{i:02}", end=" ")
+    print('\n')
+    for i in range(1):
+        print(*players[i].tehai.conv())
+        print()
+    print()
+
     print(" players - points : ho")
     for p0 in players:
         print(f"{p0.name:8s} - {p0.point:6d} : ",
@@ -62,64 +87,46 @@ if __name__ == '__main__':
     players.append(Player("CPU3"))
 
     try:
+        game.init()
         yama = hantei.rndlst(lst)
         for i in range(4):
             players[i].tehai = hantei.Tehai(sorted(yama[0:13]))
             del yama[0:13]
-        choose = random.choice(yama)
-        yama.remove(choose)
-        players[0].tehai.set(choose)
         while True:
-            if sys.platform == 'win32':
-                os.system('cls')
-            else:
-                os.system('clear')
-
-            print(f"山残り : {len(yama)}")
-            for i in range(14):
-                print(f"{i:02}", end=" ")
-            print('\n')
-            for i in range(1):
-#                 print(*[hantei.dic[x] for x in players[i].tehai])
-                print(*players[i].tehai.conv())
-                print()
-            print()
-
-            scene_game(players)
-            print("\n > ", end="")
-            usrinput = input()
-# 'q' または ':q' で終了
-            if usrinput == 'q' or usrinput == ':q':
-                break
-            if usrinput.isdigit():
-                pop = players[0].tehai.pop(int(usrinput))
-                if not pop:
-                    continue
-                players[0].ho.append(pop)
-                for AI in players[1:]:
-                    if not yama:
-                        print("山切れ")
-                        break
-                    choose = random.choice(yama)
-                    yama.remove(choose)
-                    AI.ho.append(choose)
-                    players[0].tehai.set(choose)
-                    if players[0].tehai.hantei(True, False):
-                        print("correct ロン")
-                        exit()
-                    players[0].tehai.pop(13)
-
-            else:
-                continue
-            if not yama:
-                print("山切れ")
-                break
             choose = random.choice(yama)
             yama.remove(choose)
-            players[0].tehai.set(choose)
-            if players[0].tehai.hantei(True, True):
-                print("correct 自摸和")
+            players[game.turn].tehai.set(choose)
+
+            scene_game(players)
+            if game.turn == 0:
+                if players[0].tehai.hantei(True, True):
+                    print("自摸和！")
+                    exit()
+                print("\n > ", end="")
+                usrinput = input()
+# 'q' または ':q' で終了
+                if usrinput == 'q' or usrinput == ':q':
+                    break
+                if usrinput.isdigit():
+                    pop = players[0].tehai.pop(int(usrinput))
+                    if not pop:
+                        continue
+                    players[0].ho.append(pop)
+            else:
+                pop = players[game.turn].tehai.pop(13)
+                players[game.turn].ho.append(pop)
+
+                players[0].tehai.set(pop)
+                if players[0].tehai.hantei(False, False):
+                    scene_game(players)
+                    players[0].tehai.hantei(True, False)
+                    print("放銃！")
+                    exit()
+                players[0].tehai.pop(13)
+            if len(yama)-14 == 0:
+                print("山切れ")
                 exit()
+            game.next()
 
 
 # Ctrl-C で終了
